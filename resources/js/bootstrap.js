da8b3c14165e7d1daa284b693f93a2c404336f71
NuminousBlue <jdiehl2236@gmail.com>
2023-03-01 18:34:29 -0600

    pusher socket config

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -23,7 +23,7 @@ window.Pusher = Pusher;
 window.Echo = new Echo({
     broadcaster: "pusher",
     key: import.meta.env.VITE_PUSHER_APP_KEY,
-    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? "mt1",
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? "us2",
     wsHost: import.meta.env.VITE_PUSHER_HOST
         ? import.meta.env.VITE_PUSHER_HOST
         : `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
